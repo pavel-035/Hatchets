ff4987331b7b605217dcd6679e99fe634943c2fd
pavel-035 <kdpavel8@gmail.com>
2021-12-24 15:10:15 +0500

    asd

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,8 @@ function css() {
 function images() {
     src('./src/assets/images/png/**/*.png')
         .pipe(dest('./docs/src/assets/images/png/'))
+    src('./src/assets/images/jpg/**/*.jpg')
+        .pipe(dest('./docs/src/assets/images/jpg/'))
     return src('./src/assets/images/svg/**/*.svg')
         .pipe(dest('./docs/src/assets/images/svg/'))
 }
@@ -31,6 +33,16 @@ function fonts() {
         .pipe(dest('./docs/src/assets/fonts/'))
     return src('./src/assets/fonts/**/*.ttf2')
         .pipe(dest('./docs/src/assets/fonts/'))
+}
+
+function gif() {
+    return src('./src/assets/gif/**.gif')
+        .pipe(dest('./docs/src/assets/gif/'))
+}
+
+function video() {
+    return src('./src/assets/video/**.mp4')
+        .pipe(dest('./docs/src/assets/video/'))
 }
 
 function clear() {
@@ -46,11 +58,13 @@ function serve() {
     watch('src/scripts/*.js', series(script)).on('change', sync.reload)
     watch('src/styles/*.scss', series(css)).on('change', sync.reload)
     watch('./src/assets/images/png/**/*.png', series(images)).on('change', sync.reload)
+    watch('./src/assets/gif/*.gif', series(gif)).on('change', sync.reload)
+    watch('./src/assets/video/*.mp4', series(video)).on('change', sync.reload)
     watch('./src/assets/images/svg/**/*.svg', series(images)).on('change', sync.reload)
     watch('./src/assets/fonts/**/*.ttf', series(fonts)).on('change', sync.reload)
     watch('./src/assets/fonts/**/*.ttf2', series(fonts)).on('change', sync.reload)
 }
 
-exports.build = series(clear, html, css, fonts, images, script)
-exports.serve = series(clear, html, css, fonts, images, script, serve)
+exports.build = series(clear, html, css, fonts, images, gif, video, script)
+exports.serve = series(clear, html, css, fonts, images, gif, video, script, serve)
 exports.clear = clear;
